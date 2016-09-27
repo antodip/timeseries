eaa5103122d8e3b8aa27febe27eb580448fe3f3c
antodip <adipede@techimp.com>
2016-09-27 16:57:23 +0200

    added time_series service

---
+++ time_series_service.js
@@ -0,0 +1,36 @@
+const db = require('levelup');
+
+module.exports = function(db) {
+
+    return {
+        add: function(point, cb) {
+            // var timestamp = new Date();
+            db.put(point.timestamp, point.value, function(err) {
+                   if (err)  return cb(err);
+                   cb();
+                   
+            });
+        },
+
+        points: function(start, end, cb) {
+            var ret = [];
+            db.createReadStream({
+                'gte': start,
+                'lte': end
+            }) 
+            .on('data' , function(data){
+                ret.push(data);
+            })
+            .on('err',function(err) {
+                cb(err);
+            })
+            .on('end', function(){
+                cb(null,ret);
+            })
+        
+        }
+
+    }
+
+
+}
